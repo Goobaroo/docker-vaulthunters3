db508a653e8a55d6b4d4bdad64100d77ea9ba10d
Jason McLeod <jason@mcleods.me>
2026-04-19 18:43:24 -0400

    Update to version 3.21.5

--- launch.sh
+++ launch.sh
@@ -20,9 +20,9 @@ else
 fi
 
 
-if ! [[ -f "Vault-Hunters-3rd-Edition-3.21.4.1-server-files.zip" ]]; then
+if ! [[ -f "Vault-Hunters-3rd-Edition-3.21.5-server-files.zip" ]]; then
   rm -fr config mods defaultconfigs scripts forge-*.jar start.sh *.zip
-	curl -Lo 'Vault-Hunters-3rd-Edition-3.21.4.1-server-files.zip' 'https://edge.forgecdn.net/files/7899/327/Vault-Hunters-3rd-Edition-3.21.4.1-server-files.zip' && unzip -u -o 'Vault-Hunters-3rd-Edition-3.21.4.1-server-files.zip' -d /data
+	curl -Lo 'Vault-Hunters-3rd-Edition-3.21.5-server-files.zip' 'https://edge.forgecdn.net/files/7952/467/Vault-Hunters-3rd-Edition-3.21.5-server-files.zip' && unzip -u -o 'Vault-Hunters-3rd-Edition-3.21.5-server-files.zip' -d /data
   curl -Lo forge-${FORGE_VERSION}-installer.jar 'https://maven.minecraftforge.net/net/minecraftforge/forge/'${FORGE_VERSION}'/forge-'${FORGE_VERSION}'-installer.jar'
   java -jar forge-${FORGE_VERSION}-installer.jar --installServer && rm -f forge-${FORGE_VERSION}-installer.jar
 fi
